a0c7571db698db591817af473230729f3876571b
Michael <mwooddesigns@gmail.com>
2016-11-23 15:09:46 -0500

    Practicing Vue

--- dist/js/master.js
+++ dist/js/master.js
@@ -12260,6 +12260,11 @@ Vue.component('todo-item', {
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    groceryList: [
+      {text: "Object 1"},
+      {text: "Object 2"},
+      {text: "Object 3"}
+    ]
   }
 })
--- src/js/test.js
+++ src/js/test.js
@@ -5,6 +5,11 @@ Vue.component('todo-item', {
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    groceryList: [
+      {text: "Object 1"},
+      {text: "Object 2"},
+      {text: "Object 3"}
+    ]
   }
 })
