bfd9dcfdfad4e6e21ce54c68a86a7a7546eb50c6
Michael <mwooddesigns@gmail.com>
2016-11-23 12:55:56 -0500

    Fixing build script to run on linux with a new package

--- build-scripts/concatJS.js
+++ build-scripts/concatJS.js
@@ -1,35 +1,31 @@
-var concat = require('concat-files');
+// var concat = require('concat-files');
+var buildify = require('buildify');
 
 var env = "development";
 
-var prodDependencies = ['./dependencies/velocity/velocity.min.js','./dependencies/vue/dist/vue.min.js'];
-var devDependencies = ['./dependencies/velocity/velocity.js','./dependencies/vue/dist/vue.js'];
+var prodDependencies = ['./dependencies/velocity/velocity.min.js', './dependencies/vue/dist/vue.min.js'];
+var devDependencies = ['./dependencies/velocity/velocity.js', './dependencies/vue/dist/vue.js'];
 var srcFiles = ['./src/js/test.js'];
 
-switch(env) {
-	case "development":
-	console.log("Running development concatenation.");
-	var files = devDependencies.concat(srcFiles);
-	concat([
-		'./dependencies/velocity/velocity.js',
-		'./dependencies/vue/dist/vue.js',
-		'./src/js/test.js'
-	], 'dist/js/master.js', function() {
-		console.log('dist/js/master.js created');
-	});
-	break;
-	case "production":
-	console.log("Running production concatenation.");
-	var files = prodDependencies.concat(srcFiles);
-	concat([
-		'./dependencies/velocity/velocity.min.js',
-		'./dependencies/vue/dist/vue.min.js',
-		'./src/js/test.js'
-	], 'dist/js/master.js', function() {
-		console.log('dist/js/master.js created');
-	});
-	break;
-	default:
-	console.log("An error occured: Unknown Environment");
-	break;
+switch (env) {
+  case "development":
+    console.log("Running development concatenation.");
+    var files = devDependencies.concat(srcFiles);
+
+    buildify()
+      .concat(files)
+      .save("./dist/js/master.js");
+    console.log("master.js file created");
+    break;
+  case "production":
+    console.log("Running production concatenation.");
+    var files = prodDependencies.concat(srcFiles);
+    buildify()
+      .concat(files)
+      .save("./dist/js/master.js");
+    console.log("master.js file created");
+    break;
+  default:
+    console.log("An error occured: Unknown Environment");
+    break;
 }
